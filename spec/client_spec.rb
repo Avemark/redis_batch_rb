5a9fc6a234a249711e0cd546f4242cb65d81ddb4
Christian Avemark <christian@avemark.se>
2024-01-03 19:12:22 +0100

    fix client spec

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -2,13 +2,9 @@ require "spec_helper"
 
 RSpec.describe RedisBatch::Client do
   it "Wraps a redis client" do
-    redis_client = RedisBatch::Client.configuration.redis
-    allow(redis_client).to receive(:ping).and_return('OK')
-
     ping_response = RedisBatch::Client.instance.ping
 
-    expect(RedisBatch::Client.configuration.redis).to be(redis_client)
-    expect(ping_response).to eq('OK')
-    expect(redis_client).to have_received(:ping)
+    expect(RedisBatch::Client.instance.pool).to be(RedisBatch::Client.configuration.redis)
+    expect(ping_response).to eq('PONG')
   end
 end
