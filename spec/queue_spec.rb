2c59e9ea6aacfb0f11f04353446f55eb126948ea
Christian Avemark <christian@avemark.se>
2024-01-03 19:09:20 +0100

    use connection pool style in tests too

--- spec/queue_spec.rb
+++ spec/queue_spec.rb
@@ -7,13 +7,13 @@ RSpec.describe RedisBatch::Queue do
 
     it "Adds any number of elements" do
       redis_key = "RedisBatch/add_test_queue"
-      redis_client.del(redis_key)
+      redis_client.with { |redis| redis.del(redis_key) }
       queue = RedisBatch::Queue.new("add_test")
 
       queue.add "foo"
       queue.add "bar", "baz"
 
-      queue_contents = redis_client.lrange(redis_key, 0, -1)
+      queue_contents = redis_client.with { |redis| redis.lrange(redis_key, 0, -1) }
 
       expect(queue_contents).to eq(["foo", "bar", "baz"])
     end
